e7bd770cfc8d33e14c429030ae6afcbdcbfbbc8c
mwoss <wos.mateusz16@gmail.com>
2018-05-29 00:09:39 +0200

    Hotifx preventing formation of a bottleneck in search actor

--- src/main/scala/server/StoreActor.scala
+++ src/main/scala/server/StoreActor.scala
@@ -6,6 +6,7 @@ import java.io.IOException
 import akka.actor.SupervisorStrategy.{Escalate, Restart}
 import akka.actor.{Actor, OneForOneStrategy, Props, SupervisorStrategy}
 import akka.event.Logging
+import akka.routing.FromConfig
 import message.{OrderRequest, SearchRequest, StreamRequest}
 import server.workers.order.OrderActor
 import server.workers.search.SearchActor
@@ -18,7 +19,7 @@ class StoreActor extends Actor {
 
   val logger = Logging(context.system, this)
 
-  val searchActor = context.actorOf(Props[SearchActor], "search_actor")
+  val searchActor = context.actorOf(FromConfig.props(Props[SearchActor]), "search_actor")
   val orderActor = context.actorOf(Props[OrderActor], "order_actor")
   val streamActor = context.actorOf(Props[StreamActor], "stream_actor")
 
